14d24da3761145c5f5c39fe9183ecb9e78eb8667
viniciustakedi <viniciustakedi7@gmail.com>
2020-06-26 09:52:03 -0300

    Login 99%

--- WebApiDb/VS/ExemploWebApiBd/Controllers/LoginController.cs
+++ WebApiDb/VS/ExemploWebApiBd/Controllers/LoginController.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using ExemploWebApiBd.Domains;
 using ExemploWebApiBd.Interfaces;
 using ExemploWebApiBd.Repositories;
+using ExemploWebApiBd.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -17,11 +20,50 @@ namespace ExemploWebApiBd.Controllers
     public class LoginController : ControllerBase
     {
        private IUsuarioRepository _usuarioRepository { get; set; }
+        public object JwtRegisteredClaimNames { get; private set; }
 
         public LoginController()
         {
             _usuarioRepository = new UsuarioRepository();
         }
 
+        [HttpPost]
+        public IActionResult Login(LoginViewModel Usuario)
+        {
+            UsuarioDomain usuarioSelecionado = _usuarioRepository.BuscarPorEmailSenha(Usuario.Email, Usuario.Senha);
+
+            if (usuarioSelecionado == null)
+            {
+                return NotFound("E-mail ou senha inválidos");
+            }
+
+            //payload
+
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Email, usuarioSelecionado.Email),
+                new Claim(JwtRegisteredClaimNames.Jti, usuarioSelecionado.IdUsuario.ToString()),
+                new Claim(ClaimTypes.Role, usuarioSelecionado.IdTipoUsuario.ToString())
+            };
+
+            var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes("senai-inlock-VitorLeonel-ViniciusTakedi-key-auth"));
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: "WSTower.WebApi",                // emissor do token
+                audience: "WSTower.WebApi",              // destinatário do token
+                claims: claims,                          // dados definidos acima
+                expires: DateTime.Now.AddMinutes(30),    // tempo de expiração
+                signingCredentials: creds                // credenciais do token
+            );
+
+            return Ok(new
+            {
+                token = new JwtSecurityTokenHandler().WriteToken(token)
+            });
+        }
     }
+
+}
 }
